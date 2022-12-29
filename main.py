945e22d432e8197e7eb7c7097a922f47d9010bb7
vinibretas <lawliet_chan@hotmail.com>
2022-12-29 15:35:12 -0300

    Update

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+# Script to automate some git utils
+# Imports
 import sys
+import re
+import os
+
+# Command line args
 cmdline = sys.argv
-fname = cmdline[0]
+fname = re.findall("\w*\.py$", cmdline[0])[0]
+
 args = cmdline[1:]
+
+# Constants
 DETAIL = { 
-        "subcommands: " : { 
+        "subcommands" : { 
             "add" : [
-                "`git add [message]`",
-                "If no message are given, `.` is used",
+                "`git add .`",
                 ],
 
             "commit" : [
@@ -20,13 +28,34 @@ DETAIL = {
             "a": [
                 "`git add .` folowed by `git commit -m [message]`",
                 "This flag is used alongside with `git commit`",
+                ],
+            "m": [
+                "`git commit -m`",
+                "This flag is used alongside with `git commit` too",
+                "but you don't have to put the message inside quotes"
                 ]
             },
 
         }
-            
+
+# Exception classes
+class EmptyArgsException(Exception):
+    def __init__(self):
+        usage()
+        super().__init__("NO PARAMETERS WERE GIVEN")
 
 
+class InvalidSubcommandException(Exception):
+    def __init__(self, sub):
+        usage()
+        super().__init__(f"INVALID SUBCOMMAND `{sub}`")
+
+class InvalidTagException(Exception):
+    def __init__(self, tag):
+        usage()
+        super().__init__(f"INVALID TAG `{tag}`")
+
+# Classes
 class Gutils:
     # Magic methods
     def __init__(self,args = None):
@@ -46,14 +75,52 @@ class Gutils:
         result += ")"
         return result
 
+    # Methods
     def parse_args(self, args):
+        # Set message to default, if its not updated, thats what
+        # it will be
+        msg = 'No message provided'
+        flags = None
+        
+        # Handle empty arg
         if not args: 
-            print("\n   !ERROR: No parameters were given")
-            usage()
-            exit(True)
-        subcommand, flags, msg = None, None, "No message provided"
+            raise EmptyArgsException
+
+        # Handle invalid subcommand
+        subcommand = args.pop(0)
+        if subcommand not in DETAIL["subcommands"]: 
+            raise InvalidSubcommandException(subcommand)
+
+        rest = len(args)
+        if rest > 0:
+            # Check if the first is a flag
+            first = args[0]
+            if first.startswith("-"):
+                first = first[1:]
+                if first in DETAIL["flags"]:
+                    flags = first
+                    args.pop(0)
+                else:
+                    raise InvalidTagException(first)
+            if args:
+                msg = " ".join(args)
         return (subcommand, flags, msg)
-    
+    def run(self):
+        scmd = self.subcommand
+        flags = self.flags
+        msg = self.message
+        run = "echo hello world"
+        
+        if scmd == "add":
+            run = "git add ."
+            pass
+        elif scmd == "commit":
+            pass
+        line(20)
+        print("self.run() call")
+        print(f"scmd = {scmd}\nflags = {flags}\nmsg = {msg}")
+        line(20)
+        os.system(run)
         
 
 
@@ -71,7 +138,7 @@ def usage():
     messages = [f" Usage: python3 {fname} <subcommand> [-flags] [message]"]
     # add subcommands and flags to list
     for key,value in DETAIL.items():
-        messages.append(f"{tab}{key}:")
+        messages.append(f"\n{tab}{key}:")
         for k,v in value.items():
             messages.append(f"{tab*2}{k:>6} - {v[0]}")
             for k2 in v[1:]:
@@ -103,3 +170,4 @@ def line(n, offset = 1):
 # -=- program testing -=-
 ob = Gutils(args)
 print(ob)
+ob.run()
